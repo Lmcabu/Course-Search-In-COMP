ac532864e0316621c4e9218abfb2d75fbd4451c9
Yue Gu <gcctxhl@163.com>
2021-05-03 14:58:50 +0800

    frame work of hash search

---
+++ HASH_NODE.h
@@ -0,0 +1,43 @@
+#include<string>
+#include<iostream>
+
+
+using namespace std;
+
+struct COURSE_ID {
+    string course_id;
+    COURSE_ID* next;
+};
+
+class HASH_NODE {
+    public:
+    HASH_NODE* next;
+    string getWord();
+    COURSE_ID* getCourseIDPtr();
+    HASH_NODE();
+    void setHashNode(string _word, COURSE_ID* _courseIDPtr);
+
+    private:
+    string word;
+    COURSE_ID* courseIDPtr;
+
+};
+
+
+HASH_NODE::HASH_NODE() {
+    word = "";
+    courseIDPtr = new COURSE_ID();
+}
+
+void HASH_NODE::setHashNode(string _word, COURSE_ID* _courseIDPtr) {
+    word = _word;
+    courseIDPtr = _courseIDPtr;
+}
+
+string HASH_NODE::getWord() {
+    return word;
+}
+
+COURSE_ID* HASH_NODE::getCourseIDPtr() {
+    return courseIDPtr;
+}
---
+++ Xu'ssearch.cpp
@@ -0,0 +1,64 @@
+#include <iostream> // iostream
+#include "course.h"
+#include <string>  // string
+#include <vector>
+#include <sstream> //istringstream
+#include <iostream> // cout
+#include <fstream> // ifstream
+#include "HASH_NODE.h"
+
+ 
+using namespace std;//Hello world
+
+
+const int COURSE_SIZE = 81;
+Course courseList[COURSE_SIZE];
+
+const int HASH_SIZE = 11;
+HASH_NODE* hashTable[HASH_SIZE];
+
+struct RESULT{
+    Course result_course;
+    int show_times;
+    RESULT* next;
+};
+
+string changeToLower(string source); // change a string to lower and return a new lower string
+int Hash(string word); // input a word and return the hash number of the word 
+void iniHashTable(); // initiallize the HashTable
+string* keyWordProcess(string keyword,int& wordNumber); // divided the key words into different word part and return the link of the Dynamic array
+RESULT resultSort(const RESULT &result ); // input origianl result and return the sorted result by courses show time
+void loadResult(RESULT* resultHead, const HASH_NODE* node); // input the head of result and load answer to the result
+
+
+
+
+
+
+//input the key words and return the Result link list
+
+
+RESULT *hashSearch(string keyword){
+    int wordNumber = 0;
+    RESULT* resultHead = new RESULT();
+    string* wordList = keyWordProcess(keyword, wordNumber); // return dynamic string list with lower words
+    for(int i = 0; i < wordNumber; i++) {
+        int hashNumber = Hash(wordList[i]);
+        HASH_NODE* hashHead = hashTable[hashNumber];
+        while(hashHead != NULL) {
+            if(hashHead -> getWord() == wordList[i]) {
+                loadResult(resultHead, hashHead);
+            }
+            hashHead = hashHead -> next;
+        }
+    }
+    return resultHead; 
+}
+
+
+
+
+
+int main(){
+    return 0;
+}
